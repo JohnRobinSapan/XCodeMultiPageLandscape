478948a9376ad2e23398c49d099abe65b8a290fd
John <sapanjo@sheridancollege.ca>
2020-01-21 15:33:31 -0500

    Add views

---
+++ MultiPageLandscape/View Controllers/GameViewController.swift
@@ -0,0 +1,9 @@
+//
+//  GameViewController.swift
+//  MultiPageLandscape
+//
+//  Created by John on 2020-01-21.
+//  Copyright © 2020 John. All rights reserved.
+//
+
+import Foundation
---
+++ MultiPageLandscape/View Controllers/SettingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingViewController.swift
+//  MultiPageLandscape
+//
+//  Created by John on 2020-01-21.
+//  Copyright © 2020 John. All rights reserved.
+//
+
+import Foundation
